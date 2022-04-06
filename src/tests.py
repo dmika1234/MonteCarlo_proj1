ce133744559762056de3d435c26d5d2fa84e4f81
Dominik <dmika1234@gmail.com>
2022-04-06 16:53:31 +0200

    optimizing monobit_test

--- src/tests.py
+++ src/tests.py
@@ -5,14 +5,12 @@ from scipy.stats import norm
 def monobit_test(sequence: np.ndarray, modulus: int) -> float:
     n = len(sequence)
     sn = 0
-    nr_of_bits = 0
     nr_digits = int(np.log2(modulus))
 
     for nr in sequence:
-        nr_binary_text = np.binary_repr(nr)
-        nr_of_bits = nr_of_bits + len(nr_binary_text)
+        nr_binary_text = np.binary_repr(nr, nr_digits)
         vec_min1_plus1 = 2 * np.array(list(map(int, list(nr_binary_text)))) - 1
-        sn = sn + np.sum(vec_min1_plus1) - (nr_digits - len(nr_binary_text))
+        sn = sn + np.sum(vec_min1_plus1)
 
     n_final = int(nr_digits * n)
     sn_final = sn / np.sqrt(n_final)
