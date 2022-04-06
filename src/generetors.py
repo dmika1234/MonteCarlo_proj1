915a2f025613100cea7f346f0d6c329402d2fdff
Dominik <dmika1234@gmail.com>
2022-04-06 16:51:24 +0200

    Implementing RC4

--- src/generetors.py
+++ src/generetors.py
@@ -26,7 +26,37 @@ class GLCG:
         res = np.zeros(n, dtype='int')
         res[0:self.k] = np.mod(seeds, self.modulus)
         for i in np.arange(self.k, n):
-            res[i] = np.mod(np.sum(self.coefficients * res[(i-self.k):i]), self.modulus)
+            res[i] = np.mod(np.sum(self.coefficients * res[(i - self.k):i]), self.modulus)
         return res
 
 
+class RC4:
+    def __init__(self, m: int):
+        self.m = m
+
+    def KSA(self, key) -> np.ndarray:
+        key = np.array(key)
+        key_len = len(key)
+        s = np.arange(0, self.m)
+        j = 0
+        for i in np.arange(self.m):
+            j = (j + s[i] + key[i % key_len]) % self.m
+            s[j], s[i] = s[i], s[j]  # swap values
+        return s
+
+    def PRGA(self, s: np.ndarray, n: int) -> np.ndarray:
+        i = 0
+        j = 0
+        y = np.zeros(n)
+        for r in np.arange(n):
+            i = (i + 1) % self.m
+            j = (j + s[i]) % self.m
+
+            s[i], s[j] = s[j], s[i]  # swap values
+            y[r] = s[(s[i] + s[j]) % self.m]
+        return y
+
+    def generate_numbers(self, key, n) -> np.ndarray:
+        s = self.KSA(key)
+        res = self.PRGA(s, n)
+        return res
